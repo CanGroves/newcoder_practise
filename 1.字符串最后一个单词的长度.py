722cf6b0bdc6f113eadd2841b4bcc71d8d038c94
Caner <172975487@qq.com>
2020-03-28 16:34:16 +0800

    2

--- 1.字符串最后一个单词的长度.py
+++ 1.字符串最后一个单词的长度.py
@@ -1,18 +1,19 @@
+'''
+计算字符串最后一个单词的长度，单词以空格隔开。
+输入描述:
+一行字符串，非空，长度小于5000。
+
+输出描述:
+整数N，最后一个单词的长度。
+
+示例1
+输入
+hello world
+输出
+5
+'''
+
 str_list = list(str(input()).split())
 str_last = str_list[-1]
 last_len = len(str_last)
 print(last_len)
-
-
-# Reference:
-# #coding=utf-8
-# str = raw_input("")
-# space_sky = ' '
-# if space_sky in str:
-#     str_sp = str.split(' ')
-#     print len(str_sp[-1])
-# else:
-#     print len(str)
-# ————————————————
-# 版权声明：本文为CSDN博主「开心快乐Jane」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-# 原文链接：https://blog.csdn.net/qq_25220145/java/article/details/77892792
