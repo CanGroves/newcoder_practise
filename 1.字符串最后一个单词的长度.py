a9d22b22ef197ba0f2176e23b1d780e1cbc1726e
Caner <172975487@qq.com>
2020-03-28 16:19:58 +0800

    1

---
+++ 1.字符串最后一个单词的长度.py
@@ -0,0 +1,18 @@
+str_list = list(str(input()).split())
+str_last = str_list[-1]
+last_len = len(str_last)
+print(last_len)
+
+
+# Reference:
+# #coding=utf-8
+# str = raw_input("")
+# space_sky = ' '
+# if space_sky in str:
+#     str_sp = str.split(' ')
+#     print len(str_sp[-1])
+# else:
+#     print len(str)
+# ————————————————
+# 版权声明：本文为CSDN博主「开心快乐Jane」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+# 原文链接：https://blog.csdn.net/qq_25220145/java/article/details/77892792
